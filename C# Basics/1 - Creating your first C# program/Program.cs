631366c46a72ee437e953ad2ba4862e78b84dd33
JerylDEv <jeryldestopace@gmail.com>
2019-01-28 18:27:32 +0800

    understanding the syntax of C# through hello world

---
+++ C# Basics/1 - Creating your first C# program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1___Creating_your_first_C__program
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello Jeryl!");
+    }
+  }
+}
