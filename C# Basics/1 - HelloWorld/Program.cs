3544115a51e4cdc67537813f9cdbc677bc1fc17b
JerylDEv <jeryldestopace@gmail.com>
2019-01-28 19:06:36 +0800

    learning the basics of c# through helloworld

---
+++ C# Basics/1 - HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1___HelloWorld
+{
+    class Program // Program is a Class which is a container of all the methods in our application
+    {
+        static void Main(string[] args) // Main is a Method, a block of code which has a name
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
+
