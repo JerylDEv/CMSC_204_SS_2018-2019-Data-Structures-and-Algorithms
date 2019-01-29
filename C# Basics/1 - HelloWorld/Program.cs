088edd413d675d928d6bec573270fe30619a70e6
JerylDEv <jeryldestopace@gmail.com>
2019-01-29 12:24:18 +0800

    understanding c# variables

--- C# Basics/1 - HelloWorld/Program.cs
+++ C# Basics/1 - HelloWorld/Program.cs
@@ -2,14 +2,15 @@
 
 namespace _1___HelloWorld // namespace is the name of the application. Another way of organizing our code.
 {
-  class Program // Program is a Class which is a container of all the methods in our application. Another way of organizing our code.
-  {
-    static void Main(string[] args) // Main is a Method, a block of code which has a name.
+    class Program // Program is a Class which is a container of all the methods in our application. Another way of organizing our code.
     {
-      Console.WriteLine("Hello World!"); // Console is a Class, and we are calling the WriteLine Method inside it. The way we separate lines is through ";"
-      Console.ReadLine(); // Console is a Class, and we are calling the ReadLine Method inside it. The period "." is called the "member accessor". It allows us to access a member of a Class. The parenthesis means invoke it or execute it or do it now (). It can also mean, execute the method with this argument ("Hello World!") 
+        static void Main(string[] args) // Main is a Method, a block of code which has a name.
+        {
+            Console.WriteLine("Hello World!"); // Console is a Class, and we are calling the WriteLine Method inside it. The way we separate lines is through ";"
+            Console.ReadLine(); // Console is a Class, and we are calling the ReadLine Method inside it. The period "." is called the "member accessor". It allows us to access a member of a Class. The parenthesis means invoke it or execute it or do it now (). It can also mean, execute the method with this argument ("Hello World!") 
+        }
     }
-  }
 }
+
 
 // Continue to  59:28
---
+++ C# Basics/2 - Variables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _2___Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            int x, y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+             */
+
+            Console.WriteLine("What is your name? ");
+            Console.WriteLine("Type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            string myLastName;
+            Console.WriteLine("Type your last name: ");
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {myFirstName} {myLastName}");
+            Console.ReadLine();
+
+        }
+    }
+}
