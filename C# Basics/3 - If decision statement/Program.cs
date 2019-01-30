ec17eea21ba5dfc735c7c7bad25ee60948779a92
JerylDEv <jeryldestopace@gmail.com>
2019-01-30 17:57:43 +0800

    publishing and building the console of if decision statement project

--- C# Basics/3 - If decision statement/Program.cs
+++ C# Basics/3 - If decision statement/Program.cs
@@ -6,7 +6,17 @@ namespace _3___If_decision_statement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Jeryl's Big Giveaway");
+            Console.Write("Choose a door: 1, 2, or 3: ");
+            string userValue = Console.ReadLine();
+
+            if (userValue == "1")
+            {
+                string message = "You won a house and lot!";
+                Console.WriteLine(message);
+            }
+            Console.ReadLine();
+
         }
     }
 }
