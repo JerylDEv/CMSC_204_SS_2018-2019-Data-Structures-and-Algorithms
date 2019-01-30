80328b7697101399ab5b3e8dbcc7f1cf2b84b698
JerylDEv <jeryldestopace@gmail.com>
2019-01-30 17:43:51 +0800

    learning if statement in C#

---
+++ C# Basics/3 - If decision statement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3___If_decision_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
