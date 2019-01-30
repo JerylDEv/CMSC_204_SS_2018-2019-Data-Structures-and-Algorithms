820ebc604cb442d8bd2d59636f9038d6b281045e
JerylDEv <jeryldestopace@gmail.com>
2019-01-30 18:11:20 +0800

    updating the c# code of the if decision statement project

--- C# Basics/3 - If decision statement/Program.cs
+++ C# Basics/3 - If decision statement/Program.cs
@@ -9,12 +9,27 @@ namespace _3___If_decision_statement
             Console.WriteLine("Jeryl's Big Giveaway");
             Console.Write("Choose a door: 1, 2, or 3: ");
             string userValue = Console.ReadLine();
+            string message = "";
 
             if (userValue == "1")
             {
-                string message = "You won a house and lot!";
-                Console.WriteLine(message);
+                message = "You won a house and lot!";
             }
+            else if (userValue == "2")
+            {
+                message = "You won a car!";
+            }
+            else if (userValue == "3")
+            {
+                message = "You won 1,000,000 pesos!";
+            }
+            else
+            {
+                message = "Sorry, we cannot find the number.";
+            }
+
+            Console.WriteLine(message);
+
             Console.ReadLine();
 
         }
