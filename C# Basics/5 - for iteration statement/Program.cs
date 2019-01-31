60eb21471cd04707370fbca4458dc80df1ce3d52
JerylDEv <jeryldestopace@gmail.com>
2019-01-31 21:28:14 +0800

    learning for iteration in C#

--- C# Basics/5 - for iteration statement/Program.cs
+++ C# Basics/5 - for iteration statement/Program.cs
@@ -2,11 +2,22 @@
 
 namespace _5___for_iteration_statement
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      // for iteration statement
+      for (int i = 0; i < 10; i++)
+      {
+        Console.WriteLine(i);
+        if (i == 7)
         {
-            Console.WriteLine("Hello World!");
+          Console.WriteLine("Found seven!");
+          break;
         }
+      }
+      Console.ReadLine();
+
     }
+  }
 }
