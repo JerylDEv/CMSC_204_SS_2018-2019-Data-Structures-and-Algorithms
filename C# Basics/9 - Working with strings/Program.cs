1561b262dca0572639a2ea8499cbaaf45fa7b371
JerylDEv <jeryldestopace@gmail.com>
2019-02-02 19:38:18 +0800

    learning Strings in C#

--- C# Basics/9 - Working with strings/Program.cs
+++ C# Basics/9 - Working with strings/Program.cs
@@ -44,7 +44,10 @@ namespace _9___Working_with_strings
       Console.WriteLine(myStringExample11);
 
       string myStringExample12 = " Happy Birthday to you! Happy Birthday ~ Happy Birthday ~ Happy Birthday to you!";
-
+      myStringExample12 = myStringExample12.Substring(24, 33);
+      Console.WriteLine(myStringExample12);
+      myStringExample12 = myStringExample12.ToUpper();
+      Console.WriteLine(myStringExample12);
 
       Console.ReadLine();
     }
