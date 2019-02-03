154d8ed42c13b5b377dd839cab5aca0ddea90135
JerylDEv <jeryldestopace@gmail.com>
2019-02-03 11:04:00 +0800

    working with dates and times

---
+++ C# Basics/10 - Working with dates and times/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10___Working_with_dates_and_times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
