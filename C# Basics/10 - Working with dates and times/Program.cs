8fca6e58c9a7eefe30b643778945d4b887bd4d50
JerylDEv <jeryldestopace@gmail.com>
2019-02-03 11:16:58 +0800

    learning Date and Time in C#

--- C# Basics/10 - Working with dates and times/Program.cs
+++ C# Basics/10 - Working with dates and times/Program.cs
@@ -2,11 +2,21 @@
 
 namespace _10___Working_with_dates_and_times
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      DateTime myValue = DateTime.Now;
+      Console.WriteLine($"Date and time now: {myValue.ToString()}");
+      Console.WriteLine(myValue.ToShortDateString());
+      Console.WriteLine(myValue.ToShortTimeString());
+      Console.WriteLine(myValue.ToLongDateString());
+      Console.WriteLine(myValue.ToLongTimeString());
+
+
+      Console.ReadLine();
     }
+  }
 }
+
+// paused at 4:00
