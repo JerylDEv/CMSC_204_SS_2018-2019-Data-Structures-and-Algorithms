9def81ceec9bf80a2da5a3f00db76c7542a97b37
JerylDEv <jeryldestopace@gmail.com>
2019-02-04 10:58:09 +0800

    learning dates and times in C#

--- C# Basics/10 - Working with dates and times/Program.cs
+++ C# Basics/10 - Working with dates and times/Program.cs
@@ -8,10 +8,25 @@ namespace _10___Working_with_dates_and_times
     {
       DateTime myValue = DateTime.Now;
       Console.WriteLine($"Date and time now: {myValue.ToString()}");
-      Console.WriteLine(myValue.ToShortDateString());
-      Console.WriteLine(myValue.ToShortTimeString());
-      Console.WriteLine(myValue.ToLongDateString());
-      Console.WriteLine(myValue.ToLongTimeString());
+      Console.WriteLine($"Short date string: {myValue.ToShortDateString()}");
+      Console.WriteLine($"Short time string: {myValue.ToShortTimeString()}");
+      Console.WriteLine($"Long date string: {myValue.ToLongDateString()}");
+      Console.WriteLine($"Long time string: {myValue.ToLongTimeString()}");
+
+      Console.WriteLine($"Long date string + 3 days: {myValue.AddDays(3).ToLongDateString()}");
+
+      Console.WriteLine($"Long time string + 3 hours: {myValue.AddHours(3).ToLongTimeString()}");
+
+      Console.WriteLine($"Long time string - 3 hours: {myValue.AddHours(-3).ToLongTimeString()}");
+
+      Console.WriteLine($"Current Month: {myValue.Month}");
+
+      DateTime myBirthday = new DateTime(1990, 1, 1);
+      Console.WriteLine(myBirthday.ToShortDateString());
+
+      DateTime myBirthdayVer2 = DateTime.Parse("10/01/1990");
+      TimeSpan myAge = DateTime.Now.Subtract(myBirthdayVer2);
+      Console.WriteLine($"I'm alive for {myAge.TotalDays} days.");
 
 
       Console.ReadLine();
@@ -19,4 +34,4 @@ namespace _10___Working_with_dates_and_times
   }
 }
 
-// paused at 4:00
+// completed at 4:07
