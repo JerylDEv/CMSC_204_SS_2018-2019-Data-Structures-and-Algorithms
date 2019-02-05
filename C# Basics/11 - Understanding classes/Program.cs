6a435c8bd82e19777b6a74f9013e269f15954e1f
JerylDEv <jeryldestopace@gmail.com>
2019-02-05 15:35:32 +0800

    understanding classes in C#

---
+++ C# Basics/11 - Understanding classes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _11___Understanding_classes
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
