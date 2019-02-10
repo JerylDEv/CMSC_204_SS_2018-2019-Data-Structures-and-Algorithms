3fc2f78537e3df3733b9375922d9d7391f60dae0
JerylDEv <jeryldestopace@gmail.com>
2019-02-10 10:32:39 +0800

    rework on the self-assessment questions for Arrays.

---
+++ CMSC 204 Exercises/Self-Assessment 1 - Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Self_Assessment_1___Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
