6a317ca16cfcda37aca75b51bd6248bdce7860f5
JerylDEv <jeryldestopace@gmail.com>
2019-02-10 10:25:29 +0800

    Writing code for the Array module self-assessment questions.

---
+++ C# Basics/13 - Understanding scope and access modifiers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _13___Understanding_scope_and_access_modifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
