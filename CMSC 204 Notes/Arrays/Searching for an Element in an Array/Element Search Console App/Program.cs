94d9f11f60256a4ba59721c0a5df272ebcadc9a3
JerylDEv <jeryldestopace@gmail.com>
2019-02-15 14:56:34 +0800

    adding notes about sequential search in Arrays

---
+++ CMSC 204 Notes/Arrays/Searching for an Element in an Array/Element Search Console App/Program.cs
@@ -0,0 +1,24 @@
+﻿using Element_Search_Library;
+using System;
+
+namespace Element_Search_Console_App
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ElementSearchProcedures search = new ElementSearchProcedures();
+      search.Hello();
+
+      double[] numbersList = { 11, 200, 35, 46, 500 };
+      double findTest1 = 200;
+      double findTest2 = 12;
+
+      search.SequentialSearch(numbersList, findTest1);
+      Console.WriteLine(" ");
+      search.SequentialSearch(numbersList, findTest2);
+
+      Console.ReadLine();
+    }
+  }
+}
---
+++ CMSC 204 Notes/Arrays/Searching for an Element in an Array/Element Search Library/ElementSearchProcedures.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Element_Search_Library
+{
+  public class ElementSearchProcedures
+  {
+    public void Hello()
+    {
+      Console.WriteLine("Hello Mommy Cakes");
+
+      Console.ReadLine();
+    }
+
+    public void SequentialSearch(double[] arrayInput, double number)
+    {
+      /*
+       * SequentialSearch(A,x)
+       * begin
+       *    ptr = 0;
+       *    repeat
+       *        ptr = ptr + 1;
+       *    until (A[ptr] = x) or (ptr = n);
+       *    if (A[ptr] = x) return(TRUE)
+       *    else return(FALSE)
+       * end
+       */
+
+      foreach (double element in arrayInput)
+      {
+        if (element == number) // True
+        {
+          Console.WriteLine($"Number {number} matches element {element} inside the array."); // True
+
+        }
+        else
+        {
+          Console.WriteLine($"Number {number} does not match element {element} inside the array."); // False
+        }
+      }
+    }
+
+    public void BinarySearch(double[] arrayInput, double number)
+    {
+      /*
+       *
+       *
+       *
+       *
+       *
+       *
+       */
+    }
+  }
+
+
+}
