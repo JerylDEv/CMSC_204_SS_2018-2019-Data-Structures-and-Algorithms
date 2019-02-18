e2857e551f19abc93c0758ea244a549c0fa9490a
JerylDEv <jeryldestopace@gmail.com>
2019-02-18 17:36:51 +0800

    creating a multidimensional arrays console app for SAQ 1-2

---
+++ CMSC 204 Exercises and Self-Assessments/SAQ 1-2 Multidimensional Arrays/Multidimensional Arrays Class Library/MultidimensionalArraysClassLibrary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multidimensional_Arrays_Class_Library
+{
+  public class MultidimensionalArraysClassLibrary
+  {
+    public void Hello()
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
---
+++ CMSC 204 Exercises and Self-Assessments/SAQ 1-2 Multidimensional Arrays/Multidimensional Arrays Console App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Multidimensional_Arrays_Class_Library;
+
+namespace Multidimensional_Arrays_Console_App
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      MultidimensionalArraysClassLibrary multidimensionalArray = new MultidimensionalArraysClassLibrary();
+      multidimensionalArray.Hello();
+
+      Console.ReadLine();
+    }
+  }
+}
