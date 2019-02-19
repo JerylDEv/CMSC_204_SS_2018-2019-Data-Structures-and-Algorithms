5c59c0e39836cce502c3aab343d23de94eb8fb37
JerylDEv <jeryldestopace@gmail.com>
2019-02-19 22:30:28 +0800

    created a decimal to binary class and used it in the console.

--- CMSC 204 Exercises and Self-Assessments/Representation of Information/Representation of Information Class Library/RepresentationOfInformationClassLibrary.cs
+++ CMSC 204 Exercises and Self-Assessments/Representation of Information/Representation of Information Class Library/RepresentationOfInformationClassLibrary.cs
@@ -4,5 +4,26 @@ namespace Representation_of_Information_Class_Library
 {
   public class RepresentationOfInformationClassLibrary
   {
+    public void DecimalToBinary(int number)
+    {
+      string binaryString = "";
+
+      while (number != 0)
+      {
+        if (number % 2 == 0)
+        {
+          binaryString = binaryString + "0";
+        }
+        else
+        {
+          binaryString = binaryString + "1";
+        }
+        number = number / 2;
+      }
+      char[] binaryCharArray = binaryString.ToCharArray();
+      Array.Reverse(binaryCharArray);
+      string binaryCharString = new string(binaryCharArray);
+      Console.WriteLine(binaryCharString);
+    }
   }
 }
--- CMSC 204 Exercises and Self-Assessments/Representation of Information/Representation of Information Console Application/Program.cs
+++ CMSC 204 Exercises and Self-Assessments/Representation of Information/Representation of Information Console Application/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using Representation_of_Information_Class_Library;
 
 namespace Representation_of_Information_Console_Application
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Hello World!");
+      RepresentationOfInformationClassLibrary representation = new RepresentationOfInformationClassLibrary();
+      Console.Write("Enter a Number: \t");
+      int numberInput = Convert.ToInt32(Console.ReadLine());
+      representation.DecimalToBinary(numberInput);
+
+
+      Console.ReadLine();
     }
+  }
 }
