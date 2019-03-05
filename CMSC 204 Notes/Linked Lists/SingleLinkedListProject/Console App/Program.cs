40c9c66b6162ad70f8db9ccce4aa51d3d23a9955
JerylDEv <jeryldestopace@gmail.com>
2019-03-05 15:21:45 +0800

    Creating a new project for single linked list

---
+++ CMSC 204 Notes/Linked Lists/SingleLinkedListProject/Console App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Linked_List_Class_Library;
+
+namespace Console_App
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      SingleLinkedListClass LinkedList = new SingleLinkedListClass();
+      LinkedList.Hello();
+
+      Console.ReadLine();
+    }
+  }
+}
---
+++ CMSC 204 Notes/Linked Lists/SingleLinkedListProject/Linked List Class Library/SingleLinkedListClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Linked_List_Class_Library
+{
+  public class SingleLinkedListClass
+  {
+    public void Hello()
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
